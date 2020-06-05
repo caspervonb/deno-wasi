7be4ee81faaff7ec2872eb856c6e40622dd70913
Casper Beyer <caspervonb@pm.me>
2020-06-05 20:15:27 +0800

    Add clock tests
    This adds a pair of tests to ensure that `clock_getres` and
    `clock_gettime` can be called successfully.

---
+++ testdata/c/clock_getres.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <time.h>
+
+int main() {
+	struct timespec ts;
+
+	assert(clock_getres(CLOCK_REALTIME, &ts) == 0);
+	assert(clock_getres(CLOCK_MONOTONIC, &ts) == 0);
+	assert(clock_getres(CLOCK_PROCESS_CPUTIME_ID, &ts) == 0);
+	assert(clock_getres(CLOCK_THREAD_CPUTIME_ID, &ts) == 0);
+}
