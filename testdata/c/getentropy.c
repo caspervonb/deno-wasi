12bdc310cfe01636ad883bf2f7d17ab8b09b647b
Casper Beyer <caspervonb@pm.me>
2020-06-06 02:01:23 +0800

    Add getentropy test
    This adds a test to assert that `getentropy` succeeds and has enough
    entropy.

---
+++ testdata/c/getentropy.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <unistd.h>
+
+int main() {
+	char buf[256] = {0};
+	assert(getentropy(buf, 256) == 0);
+
+	int sum = 0;
+	for (int i = 0; i < 256; i++) {
+		sum += buf[i];
+	}
+
+	assert(sum != 0);
+
+	return 0;
+}
